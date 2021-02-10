9618bf82293946e6e2df92b0ff827dce533aa70d
AMIT\AMIT <amitsalunke01@gmail.com>
2021-02-11 01:13:49 +0530

    Added custom progress bar

--- app/src/main/java/com/amitsalunke/recipeapp/RecipeListFragment.kt
+++ app/src/main/java/com/amitsalunke/recipeapp/RecipeListFragment.kt
@@ -4,6 +4,19 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.CircularProgressIndicator
+import androidx.compose.material.Text
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.platform.ContextAmbient
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.viewinterop.AndroidView
 import androidx.fragment.app.Fragment
 
 class RecipeListFragment : Fragment() {
@@ -14,8 +27,42 @@ class RecipeListFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        //when using layout .xml file and this used for custom layout
         val view = inflater.inflate(R.layout.fragment_recipe_list, container, false)
+        view.findViewById<ComposeView>(R.id.compose_view).setContent {
+            Column(
+                Modifier
+                    .border(BorderStroke(1.dp, Color.Black))
+                    .padding(16.dp)
+            ) {
+                Text("This is a composable inside the fragment")
+                Spacer(Modifier.padding(10.dp))
+                CircularProgressIndicator()
+                Spacer(Modifier.padding(10.dp))
+                Text("Neat")
+                Spacer(Modifier.padding(10.dp))
+                //custom view
+                val customView = HorizontalDottedProgress(ContextAmbient.current)
+                AndroidView(viewBlock = { customView })
+            }
+        }
         return view
+
+        //one way of writing it
+        /*val view = ComposeView(requireContext())
+        view.apply {
+            setContent {
+                Text(" Hey look a composable ")
+            }
+        }
+        return view*/
+
+        //other way
+        /*return ComposeView(requireContext()).apply {
+            setContent {
+                Text(text = "Recipe List Fragment")
+            }
+        }*/
     }
 
 
